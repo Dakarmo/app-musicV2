45f8021fbc31652b0446bcca9fe1823e84be2665
Armel DOVENON <dove.armel@gmail.com>
2023-12-23 12:47:14 +0100

    affichage des albums

--- src/app/albums/albums.component.ts
+++ src/app/albums/albums.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { ALBUMS } from '../mock-albums';
 
 @Component({
   selector: 'app-albums',
   templateUrl: './albums.component.html',
   styleUrl: './albums.component.css'
 })
+
 export class AlbumsComponent {
 
-  title = "Page Principale Albums"
+  title = "Page Principale Albums";
+  Albums = ALBUMS;
 }
