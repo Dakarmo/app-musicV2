5f7a9eeed7f4512f598953d4038e5dbf7dd253fb
Armel DOVENON <dove.armel@gmail.com>
2023-12-23 13:16:28 +0100

    importation du module MatIcone dans app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlbumsComponent } from './albums/albums.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 @NgModule({
@@ -14,7 +17,10 @@ import { AlbumsComponent } from './albums/albums.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
